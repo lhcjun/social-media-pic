b42353c482525b0b988c66f4347cc416fb9c4efa
Jun <lhcjun@gmail.com>
2020-08-08 23:21:32 +0800

    show details of logged in user in profile page, and fix layout + RWD with Gallery component

---
+++ client/src/components/gallery/gallery.component.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import './gallery.styles.scss';
+
+const Gallery = ({ userPosts }) => {    
+    const allRows = [];
+
+    const createRows = userPosts => {
+        // 3 posts in 1 row (for layout)    [[{1}, {2}, {3}], [...], [{1}]]
+        for(var i=0; i<userPosts.length; i+=3){
+            allRows.push(userPosts.slice(i, i+3));
+        }
+        return allRows;     
+    }
+
+    const checkRowLength = eachRow => {
+        let len = eachRow.length;
+
+        const pushBlankItem = id => {
+            eachRow.push({ photo: '', title: '', _id: id });
+        };
+        // make sure the last row also = 3 items (so that the img won't fill up the whole row)
+        if(len === 1){
+            pushBlankItem('blank1');
+            pushBlankItem('blank2');
+        }else if(len === 2){
+            pushBlankItem('blank1');
+        }
+        return eachRow;
+    }
+
+    return(
+      <main className='profile-gallery'>
+          <article className='gallery'>
+            {
+              createRows(userPosts).map((eachRow, i) => 
+                <div className='row' key={i}>
+                  {  
+                    checkRowLength(eachRow).map(eachPost => 
+                      <div className='img-container' key={eachPost._id}>
+                        <img 
+                            src={eachPost.photo} 
+                            alt={eachPost.title}  
+                            className='item-img' 
+                        />
+                      </div>
+                    )
+                  }
+                </div> 
+              )
+            } 
+          </article>
+      </main>
+    )
+};
+
+export default Gallery;
+
+
+
+/*  no layout
+myPosts.map(eachPost => 
+    <img src={eachPost.photo} alt={eachPost.title}  
+        className='item-img' key={eachPost._id}
+    />
+)
+*/
--- client/src/components/profile/profile.component.jsx
+++ client/src/components/profile/profile.component.jsx
@@ -1,47 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import UserContext from '../../contexts/user/user.context';
+import Gallery from '../gallery/gallery.component';
 import './profile.styles.scss';
 
-const Profile = () => (
-    <div className='profile'>
-        <div className='user-info'>
-            <img className='profile-img' src='https://images.unsplash.com/photo-1570731102433-34470efb6acf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='avatars'  />
-            <div className='introduction'>
-                <h2>Minatozaki Sana</h2>
-                <div className='user-activity'>
-                    <h4>20 posts</h4>
-                    <h4>20 followers</h4>
-                    <h4>20 following</h4>
+const Profile = () => {
+    const { state, dispatch } = useContext(UserContext);        // nearest Context.Provider
+    const { user } = state;
+    const [ myPosts, setMyPosts ] = useState([]);
+
+    useEffect(() => {
+        fetch('/myposts', {
+            headers: {'Authorization': 'Bearer ' + sessionStorage.getItem('jwt')}
+        })
+          .then(res => res.json())
+          .then(posts => setMyPosts(posts.myPosts))
+          .catch(console.log)
+    }, []);
+
+
+    return(
+        <div className='profile'>
+            <div className='user-info'>
+                <img className='profile-img' src='https://images.unsplash.com/photo-1570731102433-34470efb6acf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='avatars'  />
+                <div className='introduction'>
+                    <h2>{user ? user.name : 'Loading'}</h2>
+                    <div className='user-activity'>
+                        <h4>20 posts</h4>
+                        <h4>20 followers</h4>
+                        <h4>20 following</h4>
+                    </div>
+                    <div>Live in California</div>
                 </div>
-                <div>Live in California</div>
             </div>
+            {myPosts.length
+                ?  <Gallery userPosts={myPosts} />
+                : <p>Pending</p>
+            }
+           
         </div>
-        <div className='gallery'>
-            <article className='item'>
-                <div className='img-container'>
-                    <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='item'
-                    className='item-img' />
-                </div>
-            </article>
-            <article className='item'>
-                <div className='img-container'>
-                    <img src='https://images.unsplash.com/photo-1570731102433-34470efb6acf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='item'
-                    className='item-img' />
-                </div>
-            </article>
-            <article className='item'>
-                <div className='img-container'>
-                    <img src='https://images.unsplash.com/photo-1543762446-67600aab041f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='item'
-                    className='item-img' />
-                </div>
-            </article>
-            <article className='item'>
-                <div className='img-container'>
-                    <img src='https://images.unsplash.com/photo-1550639524-a6f58345a2ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='item'
-                    className='item-img' />
-                </div>
-            </article>
-        </div>
-    </div>
-);
+    )
+};
 
 export default Profile;
+
+
+
+
+// myPosts.map(eachPost => 
+//     <img src={eachPost.photo} alt={eachPost.title}  
+//         className='item-img' key={eachPost._id}
+//     />
+// )

ff9f13056610e14317509656a31e6451a6a38ace
Jun <lhcjun@gmail.com>
2020-08-10 23:33:50 +0800

    fix profile layout

--- client/src/components/profile/profile.component.jsx
+++ client/src/components/profile/profile.component.jsx
@@ -24,15 +24,20 @@ const Profile = () => {
                 <div className='profile-img'>
                     <img  src='https://images.unsplash.com/photo-1570731102433-34470efb6acf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='avatars'  />
                 </div>
-                <section className='introduction'>
-                    <h2>{user ? user.name : 'Loading'}</h2>
+                {/* <section className='introduction'> */}
+                    <div className='user-setting'>
+                        <h2>{user ? user.name : 'Loading'}</h2>
+                        <button className='edit-btn'>Edit Profile</button>
+                    </div>
                     <div className='user-activity'>
+                        <div className='state'>
                         <h4><span>200</span> posts</h4>
                         <h4><span>200</span> followers</h4>
                         <h4><span>200</span> following</h4>
+                        </div>
                     </div>
                     <div className='bio'>Live in California</div>
-                </section>
+                {/* </section> */}
             </header>
             {myPosts.length
                 ?  <Gallery userPosts={myPosts} />

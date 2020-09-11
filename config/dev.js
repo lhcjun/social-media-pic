a5faec5180dd23a75033d37d8669e8b0927695e4
Jun <lhcjun@gmail.com>
2020-09-12 02:33:06 +0800

    deploy setting

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGO_URI:
     'mongodb+srv://js_mg1:7OBBtdisx5qH2mC1@cluster0-qzdw9.mongodb.net/<dbname>?retryWrites=true&w=majority',
-  JWT_SECRET: 'huiyhfhkhwehe2yugug78t34p76p3fcsa',
+  JWT_SECRET: 'huiyhfhkgiow4wfjq23ymo3q4ej86gug78t34p76v8rn1awscplsa',
 };

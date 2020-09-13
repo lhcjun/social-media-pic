95a5e4f72b850cf1a1a6d94de03494513a7440bd
Jun <lhcjun@gmail.com>
2020-09-14 00:55:29 +0800

    deploy setting

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  MONGO_URI:
-    'mongodb+srv://js_mg1:7OBBtdisx5qH2mC1@cluster0-qzdw9.mongodb.net/<dbname>?retryWrites=true&w=majority',
-  JWT_SECRET: 'huiyhfhkgiow4wfjq23ymo3q4ej86gug78t34p76v8rn1awscplsa',
-  SEND_GRID_API:
-    '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',
-  EMAIL: '',
-  EMAIL_FROM: 'silhouette_auto@outlook.com',
-};

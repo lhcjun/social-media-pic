f2043002570d37cdd73e87ecad268edfd0d81933
Jun <lhcjun@gmail.com>
2020-09-18 23:52:51 +0800

    SearchUser UI

--- client/src/components/search-user/search-user.component.jsx
+++ client/src/components/search-user/search-user.component.jsx
@@ -3,14 +3,19 @@ import SearchIcon from '@material-ui/icons/Search';
 import './search-user.styles.scss';
 
 const SearchUser = () => {
-  const [email, setEmail] = useState('');
+  const [inputUser, setInputUser] = useState('');
 
   return (
     <div className="search-user">
-      <div className="search-bar">
-        <input type="search" placeholder="Who are we looking for ?" />
+      <div className={`${inputUser.length ? 'fix-bar' : ''} search-bar`}>
+        <input
+          type="search"
+          placeholder="Who are we looking for ?"
+          onChange={(e) => setInputUser(e.target.value)}
+        />
         <SearchIcon className="search-icon" />
       </div>
+      <div className="search-result">Result</div>
     </div>
   );
 };

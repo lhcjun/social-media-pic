b0357b26a677674eddd78a00a45dd01a5cf61ed0
Jun <lhcjun@gmail.com>
2020-09-26 23:55:29 +0800

    style scrollbar

--- client/src/components/view-post/view-post.component.jsx
+++ client/src/components/view-post/view-post.component.jsx
@@ -27,67 +27,74 @@ const ViewPost = () => {
       .catch(console.log);
   }, [postId]);
 
-  
   // get comments & comment number
   useEffect(() => {
-    if(eachPost.comments){
+    if (eachPost.comments) {
       setComments(eachPost.comments);
       setCommentNum(eachPost.comments.length);
     }
   }, [eachPost.comments]);
 
-
-    // user make a comment on the post
-    const setComment = (commentedPost) => {
-      setComments(commentedPost.comments);
-      setCommentNum(commentedPost.comments.length);
-    };
+  // user make a comment on the post
+  const setComment = (commentedPost) => {
+    setComments(commentedPost.comments);
+    setCommentNum(commentedPost.comments.length);
+  };
 
   return eachPost ? (
     <div className="view-post">
-      <div className="post-img-container center">
-        <img className="post-img" src={eachPost.photo} alt="post-img" />
-      </div>
-      <div className="name-row">
-        <div className="post-user">
-          <img
-            className="user-img"
-            src={eachPost.postedBy ? eachPost.postedBy.profileImg : null}
-            alt="user"
-          />
-          <div className='name-info'>
-            {eachPost.postedBy ? 
-              <Link
-                className="username"
-                to={ eachPost.postedBy._id !== user._id ? `/profile/${eachPost.postedBy._id}` : '/profile' }
-              >
-                {eachPost.postedBy.account}
-              </Link>
-              : 'Loading'}
-            <div className='posted-at'>{eachPost.createdAt ? convertTime(eachPost.createdAt) : null}</div>
+      <div className="post-layout">
+        <div className="post-img-container center">
+          <img className="post-img" src={eachPost.photo} alt="post-img" />
+        </div>
+        <div className="name-row">
+          <div className="post-user">
+            <img
+              className="user-img"
+              src={eachPost.postedBy ? eachPost.postedBy.profileImg : null}
+              alt="user"
+            />
+            <div className="name-info">
+              {eachPost.postedBy ? (
+                <Link
+                  className="username"
+                  to={eachPost.postedBy._id !== user._id? `/profile/${eachPost.postedBy._id}` : '/profile'}
+                >
+                  {eachPost.postedBy.account}
+                </Link>
+              ) : 'Loading'}
+              <div className="posted-at">
+                {eachPost.createdAt ? convertTime(eachPost.createdAt) : null}
+              </div>
+            </div>
+          </div>
+          <DeletePost eachPost={eachPost} />
+        </div>
+        <div className="post-content">
+          <h5>{eachPost.title}</h5>
+          <p>{eachPost.content}</p>
+        </div>
+        <div className="comment-area">
+          <div className="comments">
+            {comments
+              ? comments.map((eachComment) => (
+                  <div className="each-comment" key={eachComment._id}>
+                    <span className="comment-name">
+                      {eachComment.postedBy.account}
+                    </span>
+                    <span className="comment-text">{eachComment.text}</span>
+                  </div>
+                ))
+              : null}
           </div>
         </div>
-        <DeletePost eachPost={eachPost} />
+        <div className="icon">
+          {eachPost.likes ? <LikeBtn eachPost={eachPost} /> : null}
+          <ChatBubbleOutlineIcon className="comment-icon" />
+          <span>{commentNum}</span>
+        </div>
+        <CommentInput setComment={setComment} eachPost={eachPost} />
       </div>
-      <div className="post-content">
-        <h5>{eachPost.title}</h5>
-        <p>{eachPost.content}</p>
-      </div>
-      <div className="comments">
-        {comments ?
-          comments.map(eachComment => 
-            <div className="each-comment" key={eachComment._id}>
-              <span className="comment-name">{eachComment.postedBy.account}</span>
-              <span className="comment-text">{eachComment.text}</span>
-            </div>
-          ) : null}
-      </div>
-      <div className="icon">
-        {eachPost.likes ? <LikeBtn eachPost={eachPost} /> : null}
-        <ChatBubbleOutlineIcon className="comment-icon" />
-        <span>{commentNum}</span>
-      </div>
-      <CommentInput setComment={setComment} eachPost={eachPost} />
     </div>
   ) : (
     <h2>Pending</h2>
